6785db190cc9db44909e4fceeb9608b9b423d125
H <hwaltz@qq.com>
2025-03-29 17:18:44 +0800

    Update site.config.ts

--- src/site.config.ts
+++ src/site.config.ts
@@ -14,13 +14,13 @@ export const siteConfig: SiteConfig = {
 		},
 	},
 	// Used as the default description meta property and webmanifest description
-	description: "仙人掌主题",
+	description: "不要温柔地走进那个良夜",
 	// HTML lang property, found in src/layouts/Base.astro L:18 & astro.config.ts L:48
 	lang: "zh-CN",
 	// Meta property, found in src/components/BaseHead.astro L:42
 	ogLocale: "zh-CN",
 	// Used to construct the meta title property found in src/components/BaseHead.astro L:11, and webmanifest name found in astro.config.ts L:42
-	title: "仙人掌主题",
+	title: "似水年华",
 };
 
 // Used to generate links in both the Header & Footer.
@@ -41,10 +41,6 @@ export const menuLinks: { path: string; title: string }[] = [
 	{
 		path: "/notes/",
 		title: "笔记",
-	},
-  {
-		path: "/sb/",
-		title: "随笔",
 	},
 ];
 
